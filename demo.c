8475827eea73b2c84314ccc6a011b579e99152ba
Fardin Hasan Samy <fardinhasansamy@Fardins-MacBook-Air.local>
2026-09-22 12:18:48 +0600

    this is a practice task

---
+++ demo.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#define pi 3.14
+int main()
+{
+
+    // printf("Hello world");
+
+    // printf , format specifier , newline
+    // int n = 300;
+    // printf("\n");
+    // printf("my number is : %d",n);
+
+    // take age from user and show the output
+
+    // int age;
+    // printf("enter your age: ");
+    // scanf("%d",&age);
+    // printf("your age is : %d", age);
+    // return 0;
+
+    // calculator
+
+    // int a, b;
+    // printf("enter two numbers:");
+    // scanf("%d%d", &a, &b);
+   
+    // float sum = 2.0;
+    // int sub = a - b;
+    // int mul = a * b;
+    // int div = a / b;
+
+    // int rem = a % b;
+
+    // printf("sum is : %f\n", sum);
+    // printf("sub is :%d\n", sub);
+    // printf("mul is :%d\n", mul);
+    // printf("div is :%d\n", div);
+    // printf("rem is : %d",rem);
+
+
+    // char ch = 'B';
+
+    // printf("my charace is %c",ch);
+
+    // take dob of a student and show me his/her age
+
+
+    // int dob;
+    // printf("your birth year: ");
+
+    // scanf("%d",&dob);
+
+    // int age = 2026 - dob;
+
+
+    // printf("your age is : %d", age);`
+
+
+
+}
