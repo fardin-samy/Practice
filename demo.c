8763f9854864d6b7cc2e2cfd96f20ebaaa3c48d5
Fardin Hasan Samy <fardinhasansamy@Fardins-MacBook-Air.local>
2026-09-24 19:52:46 +0600

    2nd day of C

--- demo.c
+++ demo.c
@@ -23,7 +23,7 @@ int main()
     // int a, b;
     // printf("enter two numbers:");
     // scanf("%d%d", &a, &b);
-   
+
     // float sum = 2.0;
     // int sub = a - b;
     // int mul = a * b;
@@ -37,13 +37,11 @@ int main()
     // printf("div is :%d\n", div);
     // printf("rem is : %d",rem);
 
-
     // char ch = 'B';
 
     // printf("my charace is %c",ch);
 
     // take dob of a student and show me his/her age
-
 
     // int dob;
     // printf("your birth year: ");
@@ -52,9 +50,50 @@ int main()
 
     // int age = 2026 - dob;
 
-
     // printf("your age is : %d", age);`
 
+    // int myNum = 15;
+    // printf("%d",myNum);
+
+    // int x = 5, y = 6, z = 50;
+
+    // printf("%d", x+y);
+
+    // float myFloatNum = 5.99;
+    // printf("%.2f",myFloatNum);
+
+    // float a = 22 , b = 7;
+    // float divison = a / b;
+    // printf("%.4f",divison);
+
+    // char ch = 65;
+    // printf("%c", ch);
+    // printf("%c", b);
+    // printf("%c", c);
 
 
+    // const int a = 7;
+
+    // a = 9;
+
+    
+
+
+    // post increment
+    // printf("%d " , p); 
+    // printf("%d ",p++); // 5 p = 6
+    // printf("%d",p);
+
+    // pre increment
+    // int p = 5;
+    // printf("%d " , p); // 5
+    // printf("%d ",++p); // 6 6
+    // printf("%d",p);// 6
+
+    // relational operation
+
+    // printf("%d",++p);
+
+
+    // printf("%d", a);
 }
